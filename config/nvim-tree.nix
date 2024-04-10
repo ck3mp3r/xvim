11f902bfbbc376c82002e60487f2c71d5efaf5aa
Christian Kemper <christian.kemper@me.com>
2024-04-10 11:56:39 +0100

    testing nvim-tree settings

--- config/nvim-tree.nix
+++ config/nvim-tree.nix
@@ -18,8 +18,8 @@ with vimPlugins; {
     diagnostics.enable = true;
     modified.enable = true;
     prefer_startup_root = false;
-    sync_root_with_cwd = true;
-    respect_buf_cwd = false;
+    sync_root_with_cwd = false;
+    respect_buf_cwd = true;
     update_focused_file = {
       enable = true;
       update_root = false;
