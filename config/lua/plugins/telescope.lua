4d3ea2c93a32733f90b2127d7ee2c336aff07cf6
Christian Kemper <christian.kemper@me.com>
2024-09-29 13:25:46 +0100

    - Fixed formatting issues and removed unnecessary parentheses in telescope keymaps. - Reformatted Lua code blocks for readability. - Added a new keymap mapping `<C-b>` to open the Telescope buffers panel in normal mode.

--- config/lua/plugins/telescope.lua
+++ config/lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>s<leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<C-b>', ':Telescope buffers initial_mode=normal<CR>', { desc = 'Open buffers' })
 
       vim.keymap.set('n', '<leader>b/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
