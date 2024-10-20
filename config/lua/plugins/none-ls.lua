44c464d4b23406b98ecc110af8703257635ac494
Christian Kemper <christian.kemper@me.com>
2024-10-20 14:09:59 +0100

    - Update plugin configuration for no-ls, adding new sources for PyLint and Black formatting.

--- config/lua/plugins/none-ls.lua
+++ config/lua/plugins/none-ls.lua
@@ -1,11 +1,14 @@
 return {
   {
     "nvimtools/none-ls.nvim",
+    event = { "BufReadPost" },
     config = function()
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
           null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.pylint,
           null_ls.builtins.formatting.shfmt,
         }
       })
