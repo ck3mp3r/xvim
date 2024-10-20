594ca05767fce6ebd547ed91ffb482b91264e1f6
Christian Kemper <christian.kemper@me.com>
2024-10-20 14:12:45 +0100

    removing pylint

--- config/lua/plugins/none-ls.lua
+++ config/lua/plugins/none-ls.lua
@@ -8,7 +8,6 @@ return {
         sources = {
           null_ls.builtins.code_actions.gitsigns,
           null_ls.builtins.formatting.black,
-          null_ls.builtins.formatting.pylint,
           null_ls.builtins.formatting.shfmt,
         }
       })
