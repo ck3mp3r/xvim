748a09b0d37973bf5b424609468615849342c14a
Christian Kemper <christian.kemper@me.com>
2024-11-10 13:16:20 +0000

    nix flake update

--- config/lua/plugins/avante.lua
+++ config/lua/plugins/avante.lua
@@ -5,33 +5,6 @@ return {
     lazy = false,
     opts = {
       provider = 'copilot',
-      use_absolute_path = true,
-      vendors = {
-        ollama = {
-          ['local'] = true,
-          endpoint = '127.0.0.1:11434/v1',
-          model = 'llama3.2:3b',
-          parse_curl_args = function(opts, code_opts)
-            return {
-              url = opts.endpoint .. '/chat/completions',
-              headers = {
-                ['Accept'] = 'application/json',
-                ['Content-Type'] = 'application/json',
-                ['x-api-key'] = 'ollama',
-              },
-              body = {
-                model = opts.model,
-                messages = require('avante.providers').copilot.parse_message(code_opts),
-                max_tokens = 2048,
-                stream = true,
-              },
-            }
-          end,
-          parse_response_data = function(data_stream, event_state, opts)
-            require('avante.providers').openai.parse_response(data_stream, event_state, opts)
-          end,
-        },
-      },
     },
     dependencies = {
       "MunifTanjim/nui.nvim",
