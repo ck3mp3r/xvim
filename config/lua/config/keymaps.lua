d35a145ed2ed88aa4a2757470daaee1f6d92672b
Christian Kemper <christian.kemper@me.com>
2025-05-02 13:56:11 +0100

    Add CodeCompanion plugin
    - Introduces the `codecompanion-nvim` plugin for code assistance.
    - Adds keymaps `<leader>Ca` to trigger CodeCompanion Action and `<leader>Cc` to trigger CodeCompanion Chat.
    - Configures the CodeCompanion plugin with a default setup, including the action palette.
    - Adds dependencies `plenary.nvim`, `nvim-treesitter`, and `mini-pick` to the plugin list in `plugins.nix`.

--- config/lua/config/keymaps.lua
+++ config/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>Ca",
+  ":CodeCompanionAction<CR>",
+  { desc = "Action", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "<leader>Cc", ":CodeCompanionChat<CR>", { desc = "Chat", noremap = true, silent = true })
