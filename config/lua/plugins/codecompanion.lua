6cf7528c336b894b57fd33adf8e1cd750d098f50
Christian Kemper <christian.kemper@me.com>
2025-06-26 17:44:02 +0100

    feat: add mcphub extension support to codecompanion config
    - Integrate mcphub extension with custom callback and options for codecompanion
    - Enable displaying mcp tool results in chat, resource variable conversion, and slash command prompts via extension options

--- config/lua/plugins/codecompanion.lua
+++ config/lua/plugins/codecompanion.lua
@@ -4,6 +4,16 @@ return {
     event = "VeryLazy",
     config = function(_, _)
       require("codecompanion").setup({
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              show_result_in_chat = true, -- Show mcp tool results in chat
+              make_vars = true, -- Convert resources to #variables
+              make_slash_commands = true, -- Add prompts as /slash commands
+            },
+          },
+        },
         display = {
           action_palette = {
             width = 95,
@@ -19,7 +29,6 @@ return {
       })
     end,
     dependencies = {
-
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
