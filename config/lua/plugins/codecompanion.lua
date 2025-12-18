6fd4d309f372307d062a98e4e6e909fd37d40f96
Christian Kemper <christian.kemper@me.com>
2025-12-18 17:46:20 +0000

    fix: resolve mcphub extension loading error with codecompanion

--- config/lua/plugins/codecompanion.lua
+++ config/lua/plugins/codecompanion.lua
@@ -300,16 +300,6 @@ return {
 
       require("codecompanion").setup({
         adapters = adapters,
-        extensions = {
-          mcphub = {
-            callback = "mcphub.extensions.codecompanion",
-            opts = {
-              show_result_in_chat = true, -- Show mcp tool results in chat
-              make_vars = true, -- Convert resources to #variables
-              make_slash_commands = true, -- Add prompts as /slash commands
-            },
-          },
-        },
         display = {
           action_palette = {
             width = 95,
--- config/lua/plugins/mcphub.lua
+++ config/lua/plugins/mcphub.lua
@@ -1,12 +1,20 @@
 return {
   {
     "ravitemer/mcphub.nvim",
-    event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "olimorris/codecompanion.nvim",
     },
+    cmd = { "MCPHub" },
     opts = {
       cmd = vim.g.mcp_cli,
+      extensions = {
+        codecompanion = {
+          show_result_in_chat = true,
+          make_vars = true,
+          make_slash_commands = true,
+        },
+      },
     },
   },
 }
