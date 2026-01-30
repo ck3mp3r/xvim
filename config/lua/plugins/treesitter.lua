ff3428816af540d422e082d76c17626249463e26
Christian Kemper <christian.kemper@me.com>
2026-01-30 20:58:35 +0000

    Refactor treesitter plugin config and enhance features
    - Replace parser_install_dir with install_dir option
    - Use runtimepath:prepend for treesitter parsers
    - Switch to require("nvim-treesitter").setup for configuration
    - Add autocmd to start tree-sitter highlighting per buffer
    - Add autocmd to set tree-sitter indent expression per buffer

--- config/lua/plugins/treesitter.lua
+++ config/lua/plugins/treesitter.lua
@@ -4,18 +4,33 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     opts = {
-      parser_install_dir = ts_parsers,
-      ensure_installed = {},
-      auto_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+      install_dir = ts_parsers,
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function(_, opts)
-      vim.opt.runtimepath:append(ts_parsers)
-      require("nvim-treesitter.config").setup(opts)
+      vim.opt.runtimepath:prepend(ts_parsers)
+      require("nvim-treesitter").setup(opts)
+
+      -- Enable highlighting for all filetypes with tree-sitter parsers
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buf = vim.api.nvim_get_current_buf()
+          local ft = vim.bo[buf].filetype
+          if ft ~= "" then
+            pcall(vim.treesitter.start, buf)
+          end
+        end,
+      })
+
+      -- Enable indent expression for tree-sitter
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buf = vim.api.nvim_get_current_buf()
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
   },
 }
