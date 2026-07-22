220f59da6733d2c4608207978314cf47e89b96d9
Christian Kemper 🍉 <christian.kemper@pm.me>
2026-07-22 15:49:07 +0100

    Update autocmds.lua

--- config/lua/config/autocmds.lua
+++ config/lua/config/autocmds.lua
@@ -34,6 +34,6 @@ vim.api.nvim_create_autocmd("FileChangedShell", {
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "Tiltfile", "tiltfile.*" },
   callback = function()
-    vim.bo.filetype = "python"
+    vim.bo.filetype = "tiltfile"
   end,
 })
